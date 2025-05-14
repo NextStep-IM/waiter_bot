e338b44fc0de0ed27cb4efd0801785cec146234d
Haris Ali <malikharis2003@gmail.com>
2025-05-15 00:58:04 +0500

    Add input fields

--- frontend/signup.py
+++ frontend/signup.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>Sign Up</h1>", unsafe_allow_html=True)
 st.write('- - -')
+st.text_input('Enter username:', placeholder='hitchhiker42')
+st.text_input('Enter password:', type='password')
