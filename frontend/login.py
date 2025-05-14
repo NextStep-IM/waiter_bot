fdda6a11186197e816a36630725b511b400fd08b
Haris Ali <malikharis2003@gmail.com>
2025-05-15 00:45:02 +0500

    Add input fields

--- frontend/login.py
+++ frontend/login.py
@@ -3,3 +3,5 @@ import streamlit as st
 #st.title('Login')
 st.markdown("<h1 style='text-align: center;'>Login</h1>", unsafe_allow_html=True)
 st.write('- - -')
+st.text_input('Enter username: ', placeholder='hitchhiker42')
+st.text_input('Enter password: ', type='password')
