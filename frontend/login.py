e56725311c492cb05c5796a6151af5e329f6e9e1
Haris Ali <malikharis2003@gmail.com>
2025-05-15 12:55:56 +0500

    Store field objects

--- frontend/login.py
+++ frontend/login.py
@@ -3,5 +3,5 @@ import streamlit as st
 #st.title('Login')
 st.markdown("<h1 style='text-align: center;'>Login</h1>", unsafe_allow_html=True)
 st.write('- - -')
-st.text_input('Enter username: ', placeholder='hitchhiker42')
-st.text_input('Enter password: ', type='password')
+username_field = st.text_input('Enter username: ', placeholder='hitchhiker42', max_chars=20)
+password_field = st.text_input('Enter password: ', type='password', max_chars=20)
