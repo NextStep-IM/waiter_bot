b43fb3bce070a2306a924c959e9ab0b9227934ce
Haris Ali <malikharis2003@gmail.com>
2025-05-15 12:56:18 +0500

    Add Sign Up button

--- frontend/login.py
+++ frontend/login.py
@@ -1,7 +1,13 @@
 import streamlit as st
 
 #st.title('Login')
+# Login
 st.markdown("<h1 style='text-align: center;'>Login</h1>", unsafe_allow_html=True)
 st.write('- - -')
 username_field = st.text_input('Enter username: ', placeholder='hitchhiker42', max_chars=20)
 password_field = st.text_input('Enter password: ', type='password', max_chars=20)
+
+# Sign Up
+st.text('\n')
+st.markdown(':small[Don\'t have an account?]')
+signup_btn = st.button('Sign Up')
