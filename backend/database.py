e98ea3536926b3f259b14f9e08d365b3441cf1df
Haris Ali <malikharis2003@gmail.com>
2025-05-16 10:42:16 +0500

    Connect to mariadb database

---
+++ backend/database.py
@@ -0,0 +1,19 @@
+import mariadb
+
+
+class DBConnection:
+    def __init__(self):
+        try:
+            self.conn = mariadb.connect(
+                user="mysql",
+                password="dustrunner41", # TODO: Find a way to hide this
+                host="localhost",
+                port=3306,
+                database="waiter_bot_db",
+                autocommit=False,
+                reconnect=True # if connection died due to timeout or other errors.
+            )
+        except mariadb.Error as e:
+            # TODO: Try to throw the exception outside
+            pass
+
