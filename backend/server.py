9dbcfe3e233f9ce1555bdfe3af2f8c44cc0e8c31
Haris Ali <malikharis2003@gmail.com>
2025-05-16 15:05:42 +0500

    Add login endpoint

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,6 @@
+import json
 from flask import Flask, request
+from database import DBConnection
 
 
 app = Flask(__name__)
@@ -10,8 +12,24 @@ def main():
         'status': 'active'
     })
 
+db = DBConnection()
 
+@app.route('/login', methods=['POST'])
+def login():
+    name, password = request.args.values()
+    query = 'SELECT name, password FROM users WHERE name = ? AND password = ?'
+    params = (name, password)
+    try:
+        data = db.read_query(query, data=params)[0]
+    except Exception:
+        return {'error': 'Exception raised', 'status': 500}
 
+    if not data:
+        return {'auth': False, 'status': 401}
+    n, p = data
+    if not (n == name and p == password):
+        return {'auth': False, 'status': 401}
+    return {'auth': True, 'status': 200}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1111, debug=True)
