8533f0c1a689824de9b26fe2b22389d86dc19285
Haris Ali <malikharis2003@gmail.com>
2025-05-16 15:06:25 +0500

    Add login button

--- frontend/login.py
+++ frontend/login.py
@@ -5,6 +5,7 @@ st.markdown("<h1 style='text-align: center;'>Login</h1>", unsafe_allow_html=True
 st.write('- - -')
 username_field = st.text_input('Enter username: ', placeholder='hitchhiker42', max_chars=20)
 password_field = st.text_input('Enter password: ', type='password', max_chars=20)
+login_btn = st.button('Login')
 
 # Sign Up
 st.text('\n')
