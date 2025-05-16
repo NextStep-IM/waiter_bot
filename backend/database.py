9357fa7036763b04d72d93e7511dcafd762d261b
Haris Ali <malikharis2003@gmail.com>
2025-05-16 10:42:45 +0500

    Run queries with variables

--- backend/database.py
+++ backend/database.py
@@ -17,3 +17,22 @@ class DBConnection:
             # TODO: Try to throw the exception outside
             pass
 
+    def execute_query(self, query: str, data: tuple):
+        """
+        Runs a sql query
+
+        :param query: The sql query
+        :param data: The values to replace the question marks (?) in the query
+        :return: Result of the sql query
+        """
+        with self.conn.cursor(prepared=True) as cur:
+            self.conn.begin() # Start transaction
+            try:
+                cur.execute(query, data=data)
+                self.conn.commit()
+            except Exception as e:
+                # TODO: Try to throw the exception outside
+                print(e.args)
+                self.conn.rollback()
+                raise e
+
