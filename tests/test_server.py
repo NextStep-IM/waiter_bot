150a797ba40cfbb7847be2a80d04fc409ee0bd03
Haris Ali <malikharis2003@gmail.com>
2025-05-17 19:59:49 +0500

    Add unit test for login endpoint

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,6 +1,29 @@
 import pytest
-from backend.server import login
+from backend.server import app
 
 
-def test_login():
-    pass
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+@pytest.fixture
+def sample_credentials():
+    creds = {
+        'name': 'test_user_1',
+        'password': 'test_pass_1',
+    }
+    return creds.copy()
+
+def test_login(client, sample_credentials):
+    response = client.post('/login', json=sample_credentials).get_json()
+    assert response['success'] == True
+
+    sample_credentials['password'] = 'wrong_pass'
+    response = client.post('/login', json=sample_credentials).get_json()
+    assert response['success'] == False
+
+    sample_credentials['name'] = 'no_user'
+    response = client.post('/login', json=sample_credentials).get_json()
+    assert response['success'] == False
