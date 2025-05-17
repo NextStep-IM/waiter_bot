972becec4e41a2d7592263c61f134d70d2700621
Haris Ali <malikharis2003@gmail.com>
2025-05-17 19:05:37 +0500

    Improve the return value's readability and data structure

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,9 @@
 import json
+from typing import Tuple
 
 import mariadb
-from flask import Flask, request
+from flask import Flask, request, Response
+from flask import jsonify
 from database import DBConnection
 
 
@@ -9,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return json.dumps({
+    return jsonify({
         'api': 'waiter_bot',
         'status': 'active'
     })
@@ -17,21 +19,42 @@ def main():
 db = DBConnection()
 
 @app.route('/login', methods=['POST'])
-def login():
+def login() -> Tuple[Response, int]:
+    """
+    Login to the website
+
+    :return: A tuple containing the response json and status code
+    :rtype: Tuple[Response, int]
+    """
+
     name, password = request.args.values()
     query = 'SELECT name, password FROM users WHERE name = ?'
     params = (name,)
     try:
         data = db.read_query(query, data=params)[0]
     except mariadb.Error:
-        return {'error': 'Exception raised', 'status': 500}
+        return jsonify({
+            'success' :  False,
+            'message' : 'Database error'
+        }), 500
 
-    if not data[0]:
-        return {'auth': False, 'status': 401}
+    if not data[0]: # Check if database returned nothing
+        return jsonify({
+            'success': False,
+            'message': 'No such user exists'
+        }), 401
+
     n, p = data
     if not (n == name and p == password):
-        return {'auth': False, 'status': 401}
-    return {'auth': True, 'status': 200}
+        return jsonify({
+            'success': False,
+            'message': 'Invalid credentials'
+        }), 401
+
+    return jsonify({
+        'success': True,
+        'message': 'Logged in'
+    }), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1111, debug=True)
