f4b411f385d721062d5664084e6809d41593a14f
Haris Ali <malikharis2003@gmail.com>
2025-05-21 19:09:03 +0500

    Add button

--- frontend/home.py
+++ frontend/home.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.title('Hello, I am WaiterBot. At your service.')
-
+st.markdown("<h1 style='text-align: center;'>Hello!</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>I am WaiterBot!</h1>", unsafe_allow_html=True)
+st.text('\n\n')
+col1, col2, col3 = st.columns([1, 1, 1])
+with col2:
+    rec_btn = st.button('Recommend Me Recipes!')
