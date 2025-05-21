ac5fd6d6827284ec25f51b8f5f490da9dbaf1b0c
Haris Ali <malikharis2003@gmail.com>
2025-05-21 22:14:59 +0500

    Use correct path
    Depending on which directory you run in streamlit in, this will find the
    correct path.

--- frontend/recommend.py
+++ frontend/recommend.py
@@ -7,3 +7,9 @@ from backend.model import Recommender
 def load_dataframe(path) -> pd.DataFrame:
     return pd.read_csv(path)
 
+def find_dataset_path():
+    parent = Path.cwd()
+    while parent.parts[-1] != 'waiter_bot':
+        parent = parent.parent
+    return parent / 'data' / 'cleaned_recipes.csv'
+
