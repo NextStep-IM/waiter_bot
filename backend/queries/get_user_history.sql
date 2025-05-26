5122904b74dba9942999046de6179a2a12376016
Haris Ali <malikharis2003@gmail.com>
2025-05-27 00:31:51 +0500

    Get recipes' contents

---
+++ backend/queries/get_user_history.sql
@@ -0,0 +1,11 @@
+SELECT Calories, FatContent, SaturatedFatContent,
+CholesterolContent, SodiumContent, CarbohydrateContent,
+FiberContent, SugarContent, ProteinContent
+FROM recipes
+WHERE id IN (
+    SELECT recipe_id FROM user_recipes
+    WHERE user_id = (
+        SELECT id FROM users
+        WHERE name = ?
+    )
+)
