b339a0d0dd0e264567ef6e586054815bba81e65f
Haris Ali <malikharis2003@gmail.com>
2025-05-28 05:15:19 +0500

    Make function more universal

--- frontend/recommend.py
+++ frontend/recommend.py
@@ -7,11 +7,19 @@ from backend.model import Recommender
 def load_dataframe(path) -> pd.DataFrame:
     return pd.read_csv(path)
 
-def find_dataset_path():
+def find_absolute_path(filename: str):
+    """
+    Find the dataset from the main directory. Since a streamlit
+    app can be run from any directory, hard-coded paths cause FileNotFoundException.
+    This is the fix for that with the limitation that it does not go back from the main directory.
+
+    :param filename: The file to find
+    :return: Absolute path to the file
+    """
     parent = Path.cwd()
     while parent.parts[-1] != 'waiter_bot':
         parent = parent.parent
-    return parent / 'data' / 'cleaned_recipes.csv'
+    return parent / 'data' / filename
 
 df = load_dataframe(find_dataset_path())
 categories = list(df['RecipeCategory'].unique())
