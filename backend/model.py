df29a8078070e6bf290618f2676be79763af0f5c
Haris Ali <malikharis2003@gmail.com>
2025-05-30 01:27:05 +0500

    Clean

--- backend/model.py
+++ backend/model.py
@@ -1,22 +1,8 @@
-import joblib
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, FunctionTransformer
 
-
-# PIPELINE = joblib.load('recommend_recipes_pipeline.pkl')
-# DATASET: pd.DataFrame = pd.read_csv('../data/cleaned_recipes.csv')
-# def recommend_recipes(user_data: dict) -> pd.DataFrame:
-#     """
-#     Recommend recipes according to user's preferences.
-#
-#     :param user_data: Dict containing user's past chosen recipes and their contents
-#     :return: DataFrame containing the similar recipes
-#     """
-#     df_user_data = pd.DataFrame(user_data)
-#     indices = PIPELINE.transform(df_user_data)[0] # Find similar recipes
-#     return DATASET.iloc[indices]
 
 class Recommender:
 
