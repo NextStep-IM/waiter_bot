ff69cd6fb913e0d4d07b0eb4e847c74513e39869
Haris Ali <malikharis2003@gmail.com>
2025-05-30 01:31:56 +0500

    Fetch and display recipes

--- frontend/recommend.py
+++ frontend/recommend.py
@@ -1,7 +1,26 @@
-import requests
+
 import streamlit as st
 import pandas as pd
 from pathlib import Path
+
+
+# if 'save_btn_clicked' not in st.session_state:
+#     st.session_state['save_btn_clicked'] = False
+
+if 'recipes' not in st.session_state:
+    st.session_state['recipes'] = []
+
+if 'selected_recipes' not in st.session_state:
+    st.session_state['selected_recipes'] = []
+
+if 'apply_btn_clicked' not in st.session_state:
+    st.session_state['apply_btn_clicked'] = False
+
+if 'save_triggered' not in st.session_state:
+    st.session_state.save_triggered = False
+
+if 'recommended_recipes' not in st.session_state:
+    st.session_state['recommended_recipes'] = pd.DataFrame()
 
 RECIPE_DATA = ['Name', 'RecipeId', 'RecipeInstructions', 'RecipeIngredientParts', 'RecipeIngredientQuantities', 'RecipeCategory',
          'Images', 'Calories', 'FatContent', 'SaturatedFatContent', 'CholesterolContent', 'SodiumContent',
@@ -25,6 +44,48 @@ def find_absolute_path(filename: str):
         parent = parent.parent
     return parent / 'data' / filename
 
+
+
+def display_recipes(df: pd.DataFrame):
+    if not df.empty:
+        st.session_state['recipes'] = df['Name'].tolist()
+        # Index of the received dataframe may have random index numbers.
+        # This will set the index from 0 to [number of rows].
+        df.reset_index(drop=True, inplace=True)
+        no_image_path = 'assets/no_image.jpg'
+        for i in range(0, len(df), 2):
+            cols = st.columns(2)
+            for j, row in df.iloc[i:i + 2].iterrows(): # i = index, row = Series
+                print(j)
+                with cols[j % 2]:
+                    # try:
+                    #     st.image(row['Images'], use_container_width=True)
+                    # except Exception as err:
+                    #     st.image(no_image_path, use_container_width=True)
+
+                    st.markdown("---")
+                    st.subheader(f"🍽️ {row['Name']} - Details")
+                    st.markdown(f"**Category**: {row['RecipeCategory']}")
+                    st.markdown(f"**Instructions**: {row['RecipeInstructions']}")
+
+                    st.markdown(f"**Ingredients:**")
+                    parts = row['RecipeIngredientParts'].strip('[]').split(',')
+                    quantities = row['RecipeIngredientQuantities'].strip('[]').split(',')
+                    for part, qty in zip(parts, quantities):
+                        st.markdown(f"- {part}: {qty}")
+
+                    st.markdown("**Nutrition Facts**")
+                    st.markdown(f"- **Calories**: {row['Calories']} kcal")
+                    st.markdown(f"- **Fat**: {row['FatContent']}")
+                    st.markdown(f"- **Saturated Fat**: {row['SaturatedFatContent']}")
+                    st.markdown(f"- **Cholesterol**: {row['CholesterolContent']}")
+                    st.markdown(f"- **Sodium**: {row['SodiumContent']}")
+                    st.markdown(f"- **Carbs**: {row['CarbohydrateContent']}")
+                    st.markdown(f"- **Fiber**: {row['FiberContent']}")
+                    st.markdown(f"- **Sugar**: {row['SugarContent']}")
+                    st.markdown(f"- **Protein**: {row['ProteinContent']}")
+
+
 @st.cache_data
 def recommend_popular_recipes(recipes_df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -40,39 +101,25 @@ def recommend_popular_recipes(recipes_df: pd.DataFrame) -> pd.DataFrame:
     indices = merged_df.groupby('RecipeCategory')['Rating'].idxmax()
     return merged_df.iloc[indices][RECIPE_DATA]
 
-# TODO: Finish this
+
 def recommend_with_user_history():
     categories = list(recipes_df['RecipeCategory'].unique())
     selected_cat = st.selectbox('Choose a category:', ['All'] + categories)
+    result = pd.DataFrame()
+    if st.button('Apply', key='apply_btn'):
+        if selected_cat != 'All':
+            rec_response = st.session_state.http_session.post(f'http://localhost:1111/recommend/{selected_cat}')
+        else:
+            rec_response = st.session_state.http_session.post(f'http://localhost:1111/recommend/All')
 
-    if st.button('Apply'):
-        # if selected_cat != 'All':
-        #     # recipes = rec.recommend_recipes(selected_cat)
-        #     pass
-        # else:
-        #     #recipes = rec.recommend_recipes()
-        #     pass
-        pass
+        if rec_response.status_code == 200:
+            result = pd.DataFrame(rec_response.json()['message'])
+        else:
+            st.error(rec_response.text)
+        if 'save_triggered' in st.session_state:
+            st.session_state['save_triggered'] = False
 
-# TODO: Add page or dialog to show recipe's contents
-def display_recipes(df: pd.DataFrame):
-    # Index of the received dataframe may have random index numbers.
-    # This will set the index from 0 to [number of rows].
-    df.reset_index(drop=True, inplace=True)
-
-    no_image_path = 'assets/no_image.jpg'
-    for i in range(0, len(df), 3):
-        cols = st.columns(3)
-        for j, row in df.iloc[i:i + 3].iterrows(): # i = index, row = Series
-            with cols[j % 3]:
-                try:
-                    st.image(row['Images'], use_container_width=True)
-                except Exception as err:
-                    st.image(no_image_path, use_container_width=True)
-
-                st.markdown(f"<h5 style='text-align: center'>{row['Name']}</h5>", unsafe_allow_html=True)
-                if st.button(f"View Details", key=f"btn_{j}"):
-                    pass
+    display_recipes(result.copy())
 
 recipes_df = load_dataframe(find_absolute_path('cleaned_recipes.csv'))
 
@@ -80,13 +127,30 @@ response = st.session_state.http_session.get('http://localhost:1111/user')
 
 if response.status_code == 200:
     json_data = response.json()
-    print(json_data)
     if json_data['message']['first_time']:
         st.markdown(f"<h1 style='text-align: center;'>Since this is your first time, I will recommend you some popular recipes!</h1>", unsafe_allow_html=True)
         display_recipes(recommend_popular_recipes(recipes_df))
     else:
-        # TODO: Use  current user's data to get their history
-        # display_recipes(recommend_with_user_history())
-        pass
+        recommend_with_user_history()
+        if not st.session_state.save_triggered:
+            recipe_names = st.session_state['recipes']
+            selected_recipes = st.multiselect("Select recipes to save:", recipe_names)
+            if st.button("Save Selected Recipe"):
+                st.session_state.selected_recipes = selected_recipes
+                st.session_state.save_triggered = True
+                # Example action: look up full row and call your save logic
+                selected_rows = pd.DataFrame()
+                for recipe in st.session_state['selected_recipes']:
+                    print(recipe)
+                    selected_rows = recipes_df[recipes_df['Name'] == recipe]
+                selected_rows = selected_rows[RECIPE_DATA]
+                if not selected_rows.empty:
+                    json_data = selected_rows.to_dict(orient='records')
+                    print(json_data)
+                    st.session_state.http_session.post(
+                        'http://localhost:1111/save_recipe', json=json_data
+                    )
+                    st.success(f"✅ Saved: {selected_rows.to_dict()}")
+
 else:
     print('Error in response from /user')
